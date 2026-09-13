cc662898858bfb87a8534b5acb48dbfde624ad32
Growtopper Mentors <growtoppermentors@gmail.com>
2026-09-14 04:22:41 +0530

    feat: added Haryana bagless learning programme blog post

--- compress.cjs
+++ compress.cjs
@@ -11,4 +11,4 @@ async function compressImage(filePath) {
   console.log('Compressed:', filePath);
 }
 
-compressImage('public/images/og-brics.png');
+compressImage('public/images/og-haryana-bagless.png');
